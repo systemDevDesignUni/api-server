3688f3fa3fe5518bafb8c442155f857031c8c7a3
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-15 08:12:47 +0530

    create a admin model

---
+++ backend/models/admin.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import studentSchema from "express/lib/utils";
+
+const adminSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+    }
+)
+
+adminSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+studentSchema.methods.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+}
+
+export const Admin = mongoose.model("Admin", studentSchema);
