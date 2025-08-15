ae60fb9def9bd7ec843bac6c8c04313a89a3307e
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-15 09:26:53 +0530

    add auth routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/handler.js";
 import { auth } from "../middlewares/auth";
-import { studentRegister } from "../controllers/authController";
+import { studentRegister,adminLogin,student,studentLogin } from "../controllers/authController";
 
 const router = Router();
 
 router.post("/register", asyncHandler(studentRegister));
+router.post("/login", asyncHandler(studentLogin));
 
 
 export default router;
