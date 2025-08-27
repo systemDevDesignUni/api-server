43701072dc573e3ce3938f97fae76d3f042cbce5
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-27 09:48:39 +0530

    fix issue createReg

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,13 +1,13 @@
 import {Router} from "express";
 import {auth} from "../middlewares/auth.js";
 import { asyncHandler } from "../utils/handler.js";
-import {registerClassByFindStudentId, deleteRegisterClassByRegisterId, getAllRegisters, updateRegister, createRegister, registerClassByFindClassId} from "../controllers/registerController.js";
+import {registerClassByFindStudentId, deleteRegisterClassByRegisterId, getAllRegisters, updateRegister, creteStudentRegister, registerClassByFindClassId} from "../controllers/registerController.js";
 
 
 const router = Router();
 
 router.get("/registerStudentFindId", auth(true),asyncHandler(registerClassByFindStudentId));
-router.post("/createRegister", auth(true),asyncHandler(createRegister));
+router.post("/createRegister", auth(true),asyncHandler(creteStudentRegister));
 router.get("/allRegister",auth(true), asyncHandler(getAllRegisters));
 router.put("/updateRegister",auth(true), asyncHandler(updateRegister));
 router.delete("/deleteRegister",auth(true),asyncHandler(deleteRegisterClassByRegisterId));
