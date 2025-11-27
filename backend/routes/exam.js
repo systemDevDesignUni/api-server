c8da782d9c5dd336076bc8fb4d49d2b6ce156745
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-27 10:15:01 +0530

    add routes

--- backend/routes/exam.js
+++ backend/routes/exam.js
@@ -1,11 +1,15 @@
 import {Router} from "express";
 import {auth} from "../middlewares/auth.js";
 import { asyncHandler } from "../utils/handler.js";
-import {createExam, deleteExam} from "../controllers/examController.js"
+import {createExam, deleteExam, getExam, checkExamMcq} from "../controllers/examController.js"
 
 const router = Router()
-
+// add msq or delete exaa in db
 router.post("/createExam", auth(true), asyncHandler(createExam));
-router.delete("/deleteExam", auth(true), asyncHandler(deleteExam))
+router.delete("/deleteExam", auth(true), asyncHandler(deleteExam));
+
+
+router.get("/getMcqExam", auth(true), asyncHandler(getExam));
+router.post("/checkExam", auth(true), asyncHandler(checkExamMcq));
 
 export default router
